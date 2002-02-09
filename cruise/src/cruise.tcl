e6435d657bd59d60afd3cd30ea870c93f78aaf8c
klauko70 <klauko70>
2002-02-09 10:53:16 +0000

    added copyright

--- cruise/src/cruise.tcl
+++ cruise/src/cruise.tcl
@@ -1,8 +1,25 @@
+#########################################################################
+# Copyright (C) 2002 Wolfgang Schrempp & Klaus Konzept                  # 
+#                                                                       #
+# This program is free software; you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation; either version 2, or (at your option)   #
+# any later version.                                                    #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+# GNU General Public License for more details.                          #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with this program.                                              #
+#########################################################################
+
 #
 # cruise.tcl 
 # 
 
-# $Id: cruise.tcl,v 1.1 2002/02/09 10:20:32 klauko70 Exp $
+# $Id: cruise.tcl,v 1.2 2002/02/09 10:53:16 klauko70 Exp $
 #
 #
 
